b14f2bf3363e9891b3388ceb103d17f2114b6c85
pvasantala1530 <141669943+pvasantala1530@users.noreply.github.com>
2023-09-12 19:52:03 -0400

    first commit

--- src/main/java/com/skillstorm/project2/SpringBootCabinResortProject2Application.java
+++ src/main/java/com/skillstorm/project2/SpringBootCabinResortProject2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootCabinResortProject2Application {
 
+	/*first commit*/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCabinResortProject2Application.class, args);
 	}
