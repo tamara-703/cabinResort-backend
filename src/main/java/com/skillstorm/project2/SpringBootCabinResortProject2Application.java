72bb5b930080b821911b0579b9ee8e6054d53e48
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-09-12 18:09:04 -0400

    first commit on main - tsaadeh

---
+++ src/main/java/com/skillstorm/project2/SpringBootCabinResortProject2Application.java
@@ -0,0 +1,13 @@
+package com.skillstorm.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCabinResortProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCabinResortProject2Application.class, args);
+	}
+
+}
