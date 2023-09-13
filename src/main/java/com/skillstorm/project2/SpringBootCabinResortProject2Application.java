282d9149f9f28e1b93a91296836935d84cfe41dc
pvasantala1530 <pvasantala@gmail.com>
2023-09-12 20:12:53 -0400

    second

--- src/main/java/com/skillstorm/project2/SpringBootCabinResortProject2Application.java
+++ src/main/java/com/skillstorm/project2/SpringBootCabinResortProject2Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootCabinResortProject2Application {
 
-	/*first commit*/
+	/*first commit dsdsffhj*/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCabinResortProject2Application.class, args);
 	}
