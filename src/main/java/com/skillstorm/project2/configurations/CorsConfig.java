008485494bad693243d88a4561208e444ba08288
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-10-02 12:40:27 -0400

    trying to fix cors issue - try 3

--- src/main/java/com/skillstorm/project2/configurations/CorsConfig.java
+++ src/main/java/com/skillstorm/project2/configurations/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") //try https://project2-cabin-fever.s3.amazonaws.com if this fails
+                .allowedOrigins("https://project2-cabin-fever.s3.amazonaws.com") 
                 .allowedMethods("GET","POST","DELETE","PUT")
                 .allowedHeaders("*");
     }
