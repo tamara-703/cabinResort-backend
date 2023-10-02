9a6a5bc40c65bf6d6dc05a991e01044ccc1d59b1
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-10-02 10:57:00 -0400

    trying to fix cors issue - try 2

--- src/main/java/com/skillstorm/project2/configurations/CorsConfig.java
+++ src/main/java/com/skillstorm/project2/configurations/CorsConfig.java
@@ -1,16 +1,16 @@
-//package com.skillstorm.project2.configurations;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("https://project2-cabin-fever.s3.amazonaws.com/home") 
-//                .allowedMethods("*")
-//                .allowCredentials(true);
-//    }
-//}
+package com.skillstorm.project2.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") //try https://project2-cabin-fever.s3.amazonaws.com if this fails
+                .allowedMethods("GET","POST","DELETE","PUT")
+                .allowedHeaders("*");
+    }
+}
