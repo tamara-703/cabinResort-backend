2609d92782c5a158148a8ce4bf0c436a72b8343b
Tamara Saadeh <tamara.saadeh95@gmail.com>
2023-10-04 16:00:01 -0400

    changed email service class

--- src/main/java/com/skillstorm/project2/services/EmailService.java
+++ src/main/java/com/skillstorm/project2/services/EmailService.java
@@ -10,6 +10,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import com.skillstorm.project2.models.CabinLocations;
 import com.skillstorm.project2.models.Reservation;
 
 @Service
@@ -37,7 +38,6 @@ public class EmailService {
     }
     
     private String getEmailContent(Reservation reservation) {
-    	
         	
         return "<html>"
         		+ " <style>"
@@ -54,16 +54,13 @@ public class EmailService {
                 + "<h3>We are glad to inform that your cabin reservation with Cabin Fever Resorts has been confirmed!!</h3>"
                 + "<h4>Please find below the details.</h4>"
                 + "<table border='1'>"
-                + "<tr><th>Reservation number</th><th>Cabin Type</th><th>Cabin Location</th><th>Dates Booked</th><th>Amount paid</th></tr>"
-                + "<tr><td>"+reservation.getId()+"</td><td>"+reservation.getReserved_cabin_id().getCabin_name()+"</td><td>"+reservation.getReserved_cabin_id() //it was here 
+                + "<tr><th>Reservation number</th><th>Cabin Type</th><th>Guest Information</th><th>Dates Booked</th><th>Amount paid</th></tr>"
+                + "<tr><td>"+reservation.getId()+"</td><td>"+reservation.getReserved_cabin_id().getCabin_name()+"</td><td>"+reservation.getGuest_id().getEmail() 
 				+ ", "
-				+ reservation.getReserved_cabin_id()
+				+ reservation.getGuest_id().getPhone()
 		  +", "+reservation.getReserved_cabin_id()+"</td><td>"+reservation.getCheck_in()+"-"+reservation.getCheck_out()+"</td><td>$"+reservation.getReserved_cabin_id().getPrice()+"</td></tr>"
                 + "</table>"
                 + "</body></html>";
     }
     
-    //.getCabinloc().getAddress()
-    //.getCabinloc().getCity()
-    //.getCabinloc().getStateId()
 }
